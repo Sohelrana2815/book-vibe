94698307479619c68aebfa4f97b21157609f1acb
Sohelrana2815 <sohel152302@gmail.com>
2024-12-02 10:41:03 +0600

    my commit

--- src/Utility/SaveReadBook.js
+++ src/Utility/SaveReadBook.js
@@ -1,19 +1,3 @@
-const getStoredReadBooks = () => {
-  const storedReadBook = localStorage.getItem("read-book");
-  return storedReadBook ? JSON.parse(storedReadBook) : [];
-};
+// Get the stored read books from localStorage
 
-const saveReadBooks = (id) => {
-  const storedReadBooks = getStoredReadBooks();
-
-  const exist = storedReadBooks.includes(id);
-
-  if (!exist) {
-    storedReadBooks.push(id);
-    localStorage.setItem("read-book", JSON.stringify(storedReadBooks));
-    return true;
-  }
-  return false;
-};
-
-export { saveReadBooks, getStoredReadBooks };
+const 
