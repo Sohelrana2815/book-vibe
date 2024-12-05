4f3b9016b500c864de0a7b80e9df2d0ed5c3e4d3
Sohelrana2815 <sohel152302@gmail.com>
2024-12-05 12:47:32 +0600

    successfully sort by rating only read book list

--- src/Pages/ListedBooks/ListedBooks.jsx
+++ src/Pages/ListedBooks/ListedBooks.jsx
@@ -5,6 +5,9 @@ import { getStoredWishListBooks } from "../../Utility/SaveWishListBook";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 import ListedBooksCard from "./ListedBooksCard";
+import { BiSort } from "react-icons/bi";
+import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
 const ListedBooks = () => {
   // Load books data
@@ -16,11 +19,50 @@ const ListedBooks = () => {
   // Use custom hook to filter books for "Wishlist"
   const wishListBooks = useFilteredBooks(books, getStoredWishListBooks);
 
+  // Sort State Initialization
+
+  const [sortedReadBooks, setSortedReadBooks] = useState([]);
+
+  // Sort to show data after readBook changes
+
+  useEffect(() => {
+    setSortedReadBooks(readBooks);
+  }, [readBooks]);
+
+  const sortBooks = (order) => {
+    const sorted = [...readBooks].sort((a, b) => {
+      if (order === "highest") {
+        return b.rating - a.rating;
+      } else {
+        return a.rating - b.rating;
+      }
+    });
+    setSortedReadBooks(sorted);
+  };
   return (
     <>
       {/* Headline for List of Books */}
       <div className="border bg-[#131313] bg-opacity-5   rounded-2xl flex justify-center py-9">
         <h3 className="text-2xl font-bold">Books</h3>
+      </div>
+      {/* Dropdown */}
+      <div className="flex justify-center">
+        <div className="dropdown ">
+          <div tabIndex={0} role="button" className="btn m-1">
+            Sort By Rating <BiSort />
+          </div>
+          <ul
+            tabIndex={0}
+            className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
+          >
+            <li>
+              <a onClick={() => sortBooks("highest")}>Highest Rating</a>
+            </li>
+            <li>
+              <a onClick={() => sortBooks("lowest")}>Lowest Rating</a>
+            </li>
+          </ul>
+        </div>
       </div>
 
       <Tabs>
@@ -30,7 +72,7 @@ const ListedBooks = () => {
         </TabList>
         <TabPanel>
           <div className="grid gap-4">
-            {readBooks.map((book) => (
+            {sortedReadBooks.map((book) => (
               <ListedBooksCard key={book.id} book={book} />
             ))}
           </div>
