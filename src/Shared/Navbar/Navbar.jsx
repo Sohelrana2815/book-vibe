b5dd526dbc7a0391bfdd6c9d527e34f2fa7ce7a8
Sohelrana2815 <sohel152302@gmail.com>
2024-12-07 19:02:29 +0600

    added AOS Animation

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import { BiMenu } from "react-icons/bi";
 import { Link, NavLink } from "react-router-dom";
+import AosAnimation from "../../Components/AosAnimation/AosAnimation";
 
 const Navbar = () => {
   const navLinks = (
@@ -50,37 +51,39 @@ const Navbar = () => {
 
   return (
     <>
-      <div className="navbar bg-base-100 md:mt-16 mt-6">
-        <div className="navbar-start">
-          <div className="dropdown pr-2">
-            <div tabIndex={0} role="button" className="lg:hidden">
-              <BiMenu className="text-2xl" />
+      <AosAnimation animation="fade-down">
+        <div className="navbar bg-base-100 md:mt-16 mt-6">
+          <div className="navbar-start">
+            <div className="dropdown pr-2">
+              <div tabIndex={0} role="button" className="lg:hidden">
+                <BiMenu className="text-2xl" />
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+              >
+                {navLinks}
+              </ul>
             </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-            >
-              {navLinks}
-            </ul>
+            <Link to="/">
+              <span className="md:text-xl text-lg hover:text-gray-500 hover:cursor-pointer font-WorkSans">
+                Book Vibe
+              </span>
+            </Link>
           </div>
-          <Link to="/">
-            <span className="md:text-xl text-lg hover:text-gray-500 hover:cursor-pointer font-WorkSans">
-              Book Vibe
-            </span>
-          </Link>
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">{navLinks}</ul>
+          </div>
+          <div className="navbar-end flex gap-2">
+            <button className="btn md:btn-md bg-[#23be0a] text-white  font-WorkSans btn-sm">
+              Sign in
+            </button>
+            <button className="btn btn-sm md:btn-md bg-[#59c6d2] text-white font-WorkSans">
+              Sign up
+            </button>
+          </div>
         </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">{navLinks}</ul>
-        </div>
-        <div className="navbar-end flex gap-2">
-          <button className="btn md:btn-md bg-[#23be0a] text-white  font-WorkSans btn-sm">
-            Sign in
-          </button>
-          <button className="btn btn-sm md:btn-md bg-[#59c6d2] text-white font-WorkSans">
-            Sign up
-          </button>
-        </div>
-      </div>
+      </AosAnimation>
     </>
   );
 };
